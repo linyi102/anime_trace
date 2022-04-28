bd1316d6e0d41e65a9be4e68c1c47eb6b7484586
linyi <1158029271@qq.com>
2022-04-28 11:28:26 +0800

    关于版本页面：重构+外部浏览器打开地址

--- lib/scaffolds/about_version.dart
+++ lib/scaffolds/about_version.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:oktoast/oktoast.dart';
 import 'package:package_info_plus/package_info_plus.dart';
+import 'package:url_launcher/url_launcher.dart';
 // import 'package:url_launcher/url_launcher.dart';
 
 class AboutVersion extends StatefulWidget {
@@ -14,6 +15,13 @@ class AboutVersion extends StatefulWidget {
 class _AboutVersionState extends State<AboutVersion> {
   late PackageInfo packageInfo;
   bool loadOk = false;
+  final List<Uri> _uris = [
+    Uri.parse("https://github.com/linyi102/anime_trace"),
+    Uri.parse("https://gitee.com/linyi517/anime_trace"),
+    Uri.parse("https://www.wolai.com/6CcZSostD8Se5zuqfTNkAC")
+  ];
+  final List<String> _urisTitle = ["GitHub 地址", "Gitee 地址", "更新进度"];
+
   @override
   void initState() {
     super.initState();
@@ -39,61 +47,45 @@ class _AboutVersionState extends State<AboutVersion> {
         ),
       ),
       body: ListView(
-        children: [
-          ListTile(
-            title: const Text("当前版本"),
-            subtitle: loadOk ? Text(packageInfo.version) : const Text(""),
-          ),
-          ListTile(
-            title: const Text("Github 地址"),
-            subtitle: const Text(
-              "https://github.com/linyi102/anime_trace",
-              style: TextStyle(
-                overflow: TextOverflow.ellipsis,
-              ),
-            ),
-            onTap: () {
-              Clipboard.setData(const ClipboardData(
-                      text: "https://github.com/linyi102/anime_trace"))
-                  .then((value) => showToast("已复制地址"));
-            },
-          ),
-          ListTile(
-            title: const Text("Gitee 地址"),
-            subtitle: const Text(
-              "https://gitee.com/linyi517/anime_trace",
-              style: TextStyle(
-                overflow: TextOverflow.ellipsis,
-              ),
-            ),
-            onTap: () {
-              Clipboard.setData(const ClipboardData(
-                      text: "https://gitee.com/linyi517/anime_trace"))
-                  .then((value) => showToast("已复制地址"));
-            },
-          ),
-          ListTile(
-            title: const Text("更新进度"),
-            subtitle:
-                // const Text("https://www.wolai.com/6CcZSostD8Se5zuqfTNkAC"),
-                const Text(
-              "https://www.wolai.com/6CcZSostD8Se5zuqfTNkAC",
-              style: TextStyle(
-                overflow: TextOverflow.ellipsis,
-              ),
-            ),
-            onTap: () {
-              Clipboard.setData(const ClipboardData(
-                      text: "https://www.wolai.com/6CcZSostD8Se5zuqfTNkAC"))
-                  .then((value) => showToast("已复制地址"));
-            },
-          ),
-        ],
+        children: _showLVC(),
       ),
     );
   }
 
-  // void _launchURL(String urlStr) async {
-  //   if (!await launch(urlStr)) throw 'Could not launch $urlStr';
-  // }
+  _showLVC() {
+    List<Widget> lvc = [];
+    lvc.add(ListTile(
+      title: const Text("当前版本"),
+      subtitle: loadOk ? Text(packageInfo.version) : const Text(""),
+    ));
+    for (var i = 0; i < _uris.length; i++) {
+      lvc.add(
+        ListTile(
+          title: Text(_urisTitle[i]),
+          // trailing: IconButton(
+          //     onPressed: () async {
+          //       if (!await launchUrl(_uris[i])) {
+          //         throw "Could not launch $_uris[i]";
+          //       }
+          //     },
+          //     icon: const Icon(Icons.open_in_browser)),
+          subtitle: const Text(
+            "点击打开链接",
+            style: TextStyle(
+              overflow: TextOverflow.ellipsis,
+            ),
+          ),
+          onTap: () async {
+            if (!await launchUrl(_uris[i])) {
+              throw "Could not launch $_uris[i]";
+            }
+            // Clipboard.setData(const ClipboardData(
+            //         text: _uris[i]))
+            //     .then((value) => showToast("已复制地址"));
+          },
+        ),
+      );
+    }
+    return lvc;
+  }
 }
