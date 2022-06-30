1358a1c43119f8dad46da5f15a4e5dd408a66eac
linyi <1158029271@qq.com>
2022-06-30 17:40:15 +0800

    点击两次返回键退出应用

--- lib/pages/tabs.dart
+++ lib/pages/tabs.dart
@@ -4,6 +4,7 @@ import 'package:flutter_test_future/pages/directory_page.dart';
 import 'package:flutter_test_future/pages/history_page.dart';
 import 'package:flutter_test_future/pages/note_list_page.dart';
 import 'package:flutter_test_future/pages/setting_page.dart';
+import 'package:oktoast/oktoast.dart';
 
 class Tabs extends StatefulWidget {
   const Tabs({Key? key}) : super(key: key);
@@ -22,37 +23,52 @@ class _TabsState extends State<Tabs> {
     // const SettingPageTest()
   ];
   int _currentIndex = 0;
+  int _clickBackCnt = 0;
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: _list[_currentIndex],
-      // body: IndexedStack(
-      //   // 新方法，可以保持页面状态。注：历史和笔记页面无法同步更新
-      //   index: _currentIndex,
-      //   children: _list,
-      // ),
-      bottomNavigationBar: BottomNavigationBar(
-        type: BottomNavigationBarType.fixed, // 当item数量超过3个，则会显示空白，此时需要设置该属性
-        currentIndex: _currentIndex,
-        // elevation: 0,
-        // backgroundColor: const Color.fromRGBO(255, 255, 255, 1),
-        onTap: (int index) {
-          setState(() {
-            _currentIndex = index;
-          });
-        },
-        items: const [
-          BottomNavigationBarItem(icon: Icon(Icons.home_filled), label: "动漫"),
-          BottomNavigationBarItem(icon: Icon(Icons.book), label: "目录"),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.history_rounded), label: "历史"),
-          BottomNavigationBarItem(
-              // icon: Icon(Icons.note_alt_outlined),
-              icon: Icon(Icons.edit_road),
-              label: "笔记"),
-          BottomNavigationBarItem(icon: Icon(Icons.more_horiz), label: "更多"),
-        ],
+    return WillPopScope(
+      onWillPop: () async {
+        _clickBackCnt++;
+        if (_clickBackCnt == 2) {
+          return true;
+        }
+        Future.delayed(const Duration(seconds: 2)).then((value) {
+          _clickBackCnt = 0;
+          debugPrint("点击返回次数重置为0");
+        });
+        showToast("再次点击退出应用");
+        return false;
+      },
+      child: Scaffold(
+        body: _list[_currentIndex],
+        // body: IndexedStack(
+        //   // 新方法，可以保持页面状态。注：历史和笔记页面无法同步更新
+        //   index: _currentIndex,
+        //   children: _list,
+        // ),
+        bottomNavigationBar: BottomNavigationBar(
+          type: BottomNavigationBarType.fixed, // 当item数量超过3个，则会显示空白，此时需要设置该属性
+          currentIndex: _currentIndex,
+          // elevation: 0,
+          // backgroundColor: const Color.fromRGBO(255, 255, 255, 1),
+          onTap: (int index) {
+            setState(() {
+              _currentIndex = index;
+            });
+          },
+          items: const [
+            BottomNavigationBarItem(icon: Icon(Icons.home_filled), label: "动漫"),
+            BottomNavigationBarItem(icon: Icon(Icons.book), label: "目录"),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.history_rounded), label: "历史"),
+            BottomNavigationBarItem(
+                // icon: Icon(Icons.note_alt_outlined),
+                icon: Icon(Icons.edit_road),
+                label: "笔记"),
+            BottomNavigationBarItem(icon: Icon(Icons.more_horiz), label: "更多"),
+          ],
+        ),
       ),
     );
   }
