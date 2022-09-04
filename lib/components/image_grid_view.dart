ddf67456a7db33168bbf82ef1ca84003656107ac
linyi517 <1158029271@qq.com>
2022-09-04 18:35:58 +0800

    笔记页图片展示：手机端3行3列，电脑端2行6列

--- lib/components/image_grid_view.dart
+++ lib/components/image_grid_view.dart
@@ -3,18 +3,19 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter_test_future/classes/relative_local_image.dart';
 import 'package:flutter_test_future/components/image_grid_item.dart';
-// import 'package:flutter_test_future/components/error_image_builder.dart';
-// import 'package:flutter_test_future/fade_route.dart';
-// import 'package:flutter_test_future/scaffolds/image_viewer.dart';
-// import 'package:flutter_test_future/utils/image_util.dart';
 
 // 用于显示笔记图片网格
 // 使用：笔记列表页
 class ImageGridView extends StatelessWidget {
   final List<RelativeLocalImage> relativeLocalImages;
   final limitShowImageNum = true;
-  const ImageGridView({Key? key, required this.relativeLocalImages})
+
+  ImageGridView({Key? key, required this.relativeLocalImages})
       : super(key: key);
+
+  // 手机端最多显示9张，每行3个；电脑端最多显示12张，每行6个
+  int maxDisplayCount = Platform.isWindows ? 12 : 9;
+  int columnCnt = Platform.isWindows ? 6 : 3;
 
   @override
   Widget build(BuildContext context) {
@@ -22,72 +23,34 @@ class ImageGridView extends StatelessWidget {
     if (relativeLocalImages.isEmpty) {
       return Container();
     }
-    // 只有一张图片，则16/9比例显示
-    // if (relativeLocalImages.length == 1) {
-    //   return Container(
-    //     padding: const EdgeInsets.fromLTRB(10, 10, 10, 10),
-    //     child: MaterialButton(
-    //       padding: const EdgeInsets.all(0),
-    //       onPressed: () {
-    //         // Navigator.push(context, MaterialPageRoute(builder: (context) {
-    //         //   return ImageViewer(
-    //         //     relativeLocalImages: [relativeLocalImages[0]],
-    //         //     initialIndex: 0,
-    //         //   );
-    //         // }));
-    //         Navigator.push(
-    //             context,
-    //             FadeRoute(
-    //                 // 因为里面的浏览器切换图片时自带了过渡效果，所以取消这个过渡
-    //                 transitionDuration: Duration.zero,
-    //                 reverseTransitionDuration: Duration.zero,
-    //                 builder: (context) {
-    //                   return ImageViewer(
-    //                     relativeLocalImages: [relativeLocalImages[0]],
-    //                     initialIndex: 0,
-    //                   );
-    //                 }));
-    //       },
-    //       child: AspectRatio(
-    //         aspectRatio: 16 / 9, // 固定长宽
-    //         child: ClipRRect(
-    //           borderRadius: BorderRadius.circular(5), // 圆角
-    //           child: Image.file(
-    //             File(ImageUtil.getAbsoluteImagePath(
-    //                 relativeLocalImages[0].path)),
-    //             fit: BoxFit.cover,
-    //             errorBuilder: errorImageBuilder(relativeLocalImages[0].path),
-    //           ),
-    //         ),
-    //       ),
-    //     ),
-    //   );
-    // } else {
-      // 构建网格图片
-      return GridView.builder(
-        padding: const EdgeInsets.fromLTRB(15, 15, 15, 15),
-        shrinkWrap: true, // ListView嵌套GridView
-        physics: const NeverScrollableScrollPhysics(),
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: Platform.isWindows ? 9 : 3, // 横轴数量
-          crossAxisSpacing: 5, // 横轴距离
-          mainAxisSpacing: 5, // 竖轴距离
-          childAspectRatio: 1, // 网格比例。31/43为封面比例
-        ),
-        itemCount: _getGridItemCount(),
-        itemBuilder: (context, index) {
-          return ImageGridItem(
-              relativeLocalImages: relativeLocalImages,
-              initialIndex: index,
-              imageRemainCount:
-                  index == 8 ? relativeLocalImages.length - 9 : 0);
-        },
-      );
+    // 构建网格图片
+    return GridView.builder(
+      padding: const EdgeInsets.fromLTRB(15, 15, 15, 15),
+      shrinkWrap: true,
+      // ListView嵌套GridView
+      physics: const NeverScrollableScrollPhysics(),
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: columnCnt, // 横轴数量
+        crossAxisSpacing: 5, // 横轴距离
+        mainAxisSpacing: 5, // 竖轴距离
+        childAspectRatio: 1, // 网格比例。31/43为封面比例
+      ),
+      itemCount: _getGridItemCount(),
+      itemBuilder: (context, index) {
+        return ImageGridItem(
+            relativeLocalImages: relativeLocalImages,
+            initialIndex: index,
+            imageRemainCount: index == maxDisplayCount - 1
+                ? relativeLocalImages.length - maxDisplayCount
+                : 0);
+      },
+    );
     // }
   }
 
   _getGridItemCount() {
-    if (relativeLocalImages.length <= 9) return relativeLocalImages.length;
-    if (limitShowImageNum) return 9;
+    if (relativeLocalImages.length <= maxDisplayCount)
+      return relativeLocalImages.length;
+    if (limitShowImageNum) return maxDisplayCount;
   }
 }
