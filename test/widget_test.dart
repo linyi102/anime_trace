43859b8f17ea9afa731e2659eb50fdcf740c79bc
linyi517 <1158029271@qq.com>
2022-11-01 14:06:57 +0800

    关闭后台更新动漫完毕后提示(同时这样修复了更新后无法显示其他消息)

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,7 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:flutter_test_future/my_app.dart';
+import 'package:flutter_test_future/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
