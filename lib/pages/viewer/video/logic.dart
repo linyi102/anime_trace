dc5584a0c523ebe52ad7a6b286764341a8ac3f43
linyi517 <1158029271@qq.com>
2023-10-29 17:02:45 +0800

    左右拖动改变播放进度

--- lib/pages/viewer/video/logic.dart
+++ lib/pages/viewer/video/logic.dart
@@ -1,3 +1,4 @@
+import 'package:flutter_test_future/utils/time_util.dart';
 import 'package:get/get.dart';
 import 'package:media_kit/media_kit.dart';
 import 'package:media_kit_video/media_kit_video.dart';
@@ -9,8 +10,14 @@ class VideoPlayerLogic extends GetxController {
 
   VideoPlayerLogic({required this.url});
 
+  /// 长按倍速播放
   double get fastForwardRate => 3;
   bool fastForwarding = false;
+
+  /// 左右拖动进度
+  double totalDx = 0; // 左右滑动过程中累计的dx
+  int destSeconds = 0; // 最终要跳转的描述
+  String willSeekPosition = ''; // 拖动时展示的文字
 
   @override
   void onInit() {
@@ -35,6 +42,46 @@ class VideoPlayerLogic extends GetxController {
   cancelSpeedUp() {
     player.setRate(1);
     fastForwarding = false;
+    update();
+  }
+
+  calculateWillSeekPosition(double dx) {
+    totalDx += dx;
+    // 根据最终位移，计算出快进/后退的秒数
+    int secondsGap = totalDx.abs() ~/ 5;
+    // 获取当前视频播放的进度秒数
+    int curSeconds = player.state.position.inSeconds;
+    // 要跳转的进度秒数
+    destSeconds = curSeconds;
+
+    if (totalDx > 0) {
+      destSeconds += secondsGap;
+      // 如果快进到最大秒数，则修正为最大秒数
+      if (destSeconds > player.state.duration.inSeconds) {
+        destSeconds = player.state.duration.inSeconds;
+      }
+
+      willSeekPosition =
+          "${TimeUtil.getReadableDuration(Duration(seconds: destSeconds))}\n[+${TimeUtil.getReadableDuration(Duration(seconds: secondsGap))}]";
+    } else if (totalDx < 0) {
+      destSeconds -= secondsGap;
+      // 如果后退至开头，则修正为0
+      if (destSeconds < 0) destSeconds = 0;
+
+      willSeekPosition =
+          "${TimeUtil.getReadableDuration(Duration(seconds: destSeconds))}\n[-${TimeUtil.getReadableDuration(Duration(seconds: secondsGap))}]";
+    }
+    update();
+  }
+
+  seekDragEndPosition() async {
+    // 寻找并播放
+    await player.seek(Duration(seconds: destSeconds));
+    player.play();
+    // 重置
+    totalDx = 0;
+    destSeconds = 0;
+    willSeekPosition = '';
     update();
   }
 }
--- lib/pages/viewer/video/view.dart
+++ lib/pages/viewer/video/view.dart
@@ -42,35 +42,66 @@ class VideoPlayerPageState extends State<VideoPlayerPage> {
         child: GestureDetector(
             onLongPressStart: (details) => logic.longPressToSpeedUp(),
             onLongPressUp: () => logic.cancelSpeedUp(),
+            onHorizontalDragStart: (details) => logic.player.pause(),
+            onHorizontalDragUpdate: (details) =>
+                logic.calculateWillSeekPosition(details.delta.dx),
+            onHorizontalDragEnd: (details) => logic.seekDragEndPosition(),
             child: Stack(
               children: [
-                MultiPlatform(
-                  mobile: MaterialVideoControlsTheme(
-                    normal: MaterialVideoControlsThemeData(
-                      topButtonBar: _buildTopBar(context),
-                    ),
-                    fullscreen: MaterialVideoControlsThemeData(
-                      topButtonBar: _buildTopBar(context),
-                    ),
-                    child: _buildVideoView(),
-                  ),
-                  desktop: MaterialDesktopVideoControlsTheme(
-                    normal: MaterialDesktopVideoControlsThemeData(
-                      topButtonBar: _buildTopBar(context),
-                    ),
-                    fullscreen: MaterialDesktopVideoControlsThemeData(
-                      topButtonBar: _buildTopBar(context),
-                    ),
-                    child: _buildVideoView(),
-                  ),
-                ),
+                _buildMultiPlatformVideoView(context),
                 _buildFastForwarding(),
+                _buildDragSeekPosition(),
               ],
             )),
       ),
     );
   }
 
+  /// 左右拖动改变进度位置
+  _buildDragSeekPosition() {
+    if (logic.willSeekPosition.isEmpty) return const SizedBox();
+    return Align(
+      alignment: Alignment.center,
+      child: Text(
+        logic.willSeekPosition,
+        style: const TextStyle(
+            color: Colors.white,
+            fontSize: 30,
+            fontWeight: FontWeight.w600,
+            shadows: [
+              Shadow(blurRadius: 3, color: Colors.black),
+            ]),
+        textAlign: TextAlign.center,
+      ),
+    );
+  }
+
+  MultiPlatform _buildMultiPlatformVideoView(BuildContext context) {
+    return MultiPlatform(
+      mobile: MaterialVideoControlsTheme(
+        normal: MaterialVideoControlsThemeData(
+          topButtonBar: _buildTopBar(context),
+          volumeGesture: true,
+        ),
+        fullscreen: MaterialVideoControlsThemeData(
+          topButtonBar: _buildTopBar(context),
+          volumeGesture: true,
+        ),
+        child: _buildVideoView(),
+      ),
+      desktop: MaterialDesktopVideoControlsTheme(
+        normal: MaterialDesktopVideoControlsThemeData(
+          topButtonBar: _buildTopBar(context),
+        ),
+        fullscreen: MaterialDesktopVideoControlsThemeData(
+          topButtonBar: _buildTopBar(context),
+        ),
+        child: _buildVideoView(),
+      ),
+    );
+  }
+
+  /// 长按倍速播放
   _buildFastForwarding() {
     if (!logic.fastForwarding) return const SizedBox();
     return Align(
@@ -96,17 +127,29 @@ class VideoPlayerPageState extends State<VideoPlayerPage> {
         children: [
           IconButton(
             onPressed: () => Navigator.pop(context),
-            icon: const Icon(Icons.arrow_back_ios_rounded, size: 20),
+            icon: Icon(
+              Icons.arrow_back_ios_rounded,
+              size: 20,
+              color: Colors.white,
+              shadows: _shadows,
+            ),
           ),
           const SizedBox(width: 15),
           Text(
             title,
-            style: const TextStyle(fontSize: 20),
+            style: TextStyle(
+              fontSize: 20,
+              color: Colors.white,
+              shadows: _shadows,
+            ),
           ),
         ],
       )
     ];
   }
+
+  List<Shadow> get _shadows =>
+      [const Shadow(blurRadius: 3, color: Colors.black)];
 
   Video _buildVideoView() => Video(controller: logic.videoController);
 }
