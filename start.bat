f0f017819ce5ca7c350820551e56736cbd294682
linyi517 <1158029271@qq.com>
2024-01-13 15:15:05 +0800

    ci: 移除多余脚本

--- start.bat
+++
@@ -1,4 +0,0 @@
-D:\"Program Files (x86)"\MuMu9\emulator\nemu9\vmonitor\bin\adb_server.exe connect 127.0.0.1:7555
-@REM flutter run
-@REM flutter run --enable-software-rendering
-@REM --no-sound-null-safety 
