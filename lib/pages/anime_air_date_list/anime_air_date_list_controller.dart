a96e996ad0130f41c7bc42fcf2119eb6c0032ff1
linyi517 <1158029271@qq.com>
2024-03-17 17:35:52 +0800

    feat: 放映时间条目里的动漫按放映时间顺序排序

--- lib/pages/anime_air_date_list/anime_air_date_list_controller.dart
+++ lib/pages/anime_air_date_list/anime_air_date_list_controller.dart
@@ -38,10 +38,17 @@ class AnimeAirDateListController extends GetxController {
     animeAirDateTimeItems.addAll(timeMapAnime.keys.map(
       (e) => AnimeAirDateItem(time: e, animes: timeMapAnime[e] ?? []),
     ));
+    // 倒序展示放映时间条目
     animeAirDateTimeItems.sort((a, b) => -a.time.compareTo(b.time));
+    // 放映时间条目里的动漫按放映时间顺序排序
+    for (final item in animeAirDateTimeItems) {
+      item.animes.sort((a, b) => a.premiereTime.compareTo(b.premiereTime));
+    }
+    // 开头添加最近观看条目
     animeAirDateTimeItems.insert(0, recentWatchedAnimeDateItem);
+    // 生成所有放映时间，便于快速跳转
     allAirDate.addAll(animeAirDateTimeItems.map((e) => e.time));
-
+    // 获取最近观看动漫
     await _loadRecentWatchedAnimes();
     update();
   }
