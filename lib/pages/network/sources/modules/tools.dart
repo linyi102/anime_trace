8f8f9ecd1797595221af91a4507062ef5361a834
linyi517 <1158029271@qq.com>
2024-07-07 20:40:41 +0800

    feat: 工具栏移除番组放送

--- lib/pages/network/sources/modules/tools.dart
+++ lib/pages/network/sources/modules/tools.dart
@@ -6,22 +6,11 @@ import 'package:flutter_test_future/pages/settings/image_wall/note_image_wall.da
 import 'package:flutter_test_future/routes/get_route.dart';
 import 'package:ming_cute_icons/ming_cute_icons.dart';
 
-import '../../../../components/website_logo.dart';
-import '../../../../models/fav_website.dart';
-import '../../../../utils/launch_uri_util.dart';
 import '../pages/dedup/dedup_page.dart';
 import '../pages/trace/view.dart';
 
 class ToolsPage extends StatelessWidget {
   const ToolsPage({super.key});
-
-  double get buttonSize => 40.0;
-  double get itemHeight => 100.0;
-  double get itemWidth => 120.0;
-  get favWebsite => FavWebsite(
-      url: "https://bgmlist.com/",
-      icoUrl: "assets/images/website/fzff.png",
-      name: "番组放送");
 
   @override
   Widget build(BuildContext context) {
@@ -34,12 +23,6 @@ class ToolsPage extends StatelessWidget {
       child: Wrap(
         spacing: 4,
         children: [
-          GestureDetector(
-              onTap: () => _openFanZuFangSong(context),
-              child: Chip(
-                  avatar: WebSiteLogo(url: favWebsite.icoUrl, size: 18),
-                  // avatar: Icon(Icons.view_week),
-                  label: const Text('番组放送'))),
           GestureDetector(
               onTap: () => _toDedupPage(context),
               child: const Chip(
@@ -92,7 +75,4 @@ class ToolsPage extends StatelessWidget {
     Navigator.of(context)
         .push(MaterialPageRoute(builder: (context) => const DedupPage()));
   }
-
-  _openFanZuFangSong(BuildContext context) =>
-      LaunchUrlUtil.launch(context: context, uriStr: favWebsite.url);
 }
