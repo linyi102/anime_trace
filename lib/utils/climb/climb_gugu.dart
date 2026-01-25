7ca74a832b64a6401229127e9e107d27937f10bb
linyi517 <1158029271@qq.com>
2026-01-25 18:16:12 +0800

    fix: 修复咕咕番无法获取名字和详情

--- lib/utils/climb/climb_gugu.dart
+++ lib/utils/climb/climb_gugu.dart
@@ -1,6 +1,7 @@
 import 'package:animetrace/models/anime.dart';
+import 'package:animetrace/models/enum/play_status.dart';
 import 'package:animetrace/utils/climb/climb.dart';
-import 'package:animetrace/utils/climb/same_ui/climb_cyc_ui.dart';
+import 'package:animetrace/utils/regexp.dart';
 
 class ClimbGugu with Climb {
   @override
@@ -16,7 +17,52 @@ class ClimbGugu with Climb {
   Future<Anime> climbAnimeInfo(Anime anime) async {
     final document = await dioGetAndParse(anime.animeUrl);
     if (document == null) return anime;
-    return CycUIClimber.detail(document, anime, playStatusElementIndex: 0);
+
+    anime.animeCoverUrl = document
+            .getElementsByClassName('detail-pic')
+            .firstOrNull
+            ?.getElementsByTagName('img')
+            .firstOrNull
+            ?.attributes["data-src"]
+            ?.replaceFirst(RegExp(r'.*url='), '') ??
+        anime.animeCoverUrl;
+
+    String parseMoreItemValue(String? html) {
+      if (html == null) return '';
+
+      return html.replaceFirst(RegExp(r'<em.*<\/em>'), '');
+    }
+
+    final moreEs = document
+            .getElementsByClassName('gen-search-form')
+            .firstOrNull
+            ?.getElementsByTagName('li') ??
+        [];
+
+    String? findMoreItemE(String key) {
+      for (final itemE in moreEs) {
+        if (itemE.innerHtml.contains(key)) {
+          return itemE.innerHtml.replaceFirst(RegExp(r'<em.*<\/em>'), '');
+        }
+      }
+      return null;
+    }
+
+    anime.area = findMoreItemE('地区：') ?? anime.area;
+    anime.premiereTime =
+        RegexpUtil.extractDate(parseMoreItemValue(findMoreItemE('上映：'))) ??
+            anime.premiereTime;
+    anime.playStatus =
+        PlayStatus.text2PlayStatus(findMoreItemE('状态：') ?? '').text;
+    anime.animeDesc = findMoreItemE('简介：') ?? anime.animeDesc;
+    anime.animeEpisodeCnt = document
+            .getElementsByClassName("anthology-list-box")
+            .firstOrNull
+            ?.getElementsByTagName("li")
+            .length ??
+        anime.animeEpisodeCnt;
+
+    return anime;
   }
 
   @override
@@ -24,6 +70,40 @@ class ClimbGugu with Climb {
     String url = baseUrl + "/index.php/vod/search.html?wd=$keyword";
     final document = await dioGetAndParse(url);
     if (document == null) return [];
-    return CycUIClimber.search(document, baseUrl);
+    List<Anime> animes = [];
+
+    final listEs = document.getElementsByClassName('search-list');
+    for (final itemE in listEs) {
+      String? coverUrl =
+          itemE.getElementsByTagName('img').firstOrNull?.attributes['data-src'];
+      if (coverUrl == null) continue;
+      if (coverUrl.startsWith('//')) coverUrl = 'https:$coverUrl';
+      coverUrl = coverUrl.replaceFirst(RegExp(r'.*url='), '');
+
+      String? name = itemE
+          .getElementsByClassName('slide-info-title')
+          .firstOrNull
+          ?.innerHtml;
+      if (name == null) continue;
+
+      String? detailUrl = itemE
+          .getElementsByClassName('detail-info')
+          .firstOrNull
+          ?.getElementsByTagName('a')
+          .firstOrNull
+          ?.attributes['href'];
+      if (detailUrl == null) continue;
+      if (!detailUrl.startsWith('http')) detailUrl = baseUrl + detailUrl;
+
+      animes.add(
+        Anime(
+          animeName: name,
+          animeCoverUrl: coverUrl,
+          animeUrl: detailUrl,
+        ),
+      );
+    }
+
+    return animes;
   }
 }
